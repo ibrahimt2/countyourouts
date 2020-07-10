273e81c782710b78c9d44792477f960841f27b49
Ibrahim T. Miraj <zerobottles@outlook.com>
2020-07-10 11:28:45 -0400

    began work on adding auth0 authentication

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-let cardSrc = require('./utils/cardSrc.js')
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+let cardSrc = require("./utils/cardSrc.js");
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    
+    <Auth0Provider
+      domain="dev-6wl79wos.us.auth0.com"
+      clientId="9BCL4264SN2jBqjjXPx8aWmGBJxS2sJx"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
