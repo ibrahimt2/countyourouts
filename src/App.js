dac9cdced882802724ac74b2ac1d834c518007ec
Ibrahim T. Miraj <zerobottles@outlook.com>
2020-07-11 07:38:57 -0400

    adjusted container margins

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <React.Fragment>
       <Router>
         <NavigationBar />
-        <Container>
+        <Container fluid className="m-0 p-0">
         <Row>
           <Col xs={0} sm={2} md={3} lg={3} xl={3}></Col>
           <Col className="m-1">
